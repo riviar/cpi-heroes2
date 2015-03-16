6adaae3aa3527b9fa2371615c130c2b55cce4767
Fox <Fox@Sigil>
2015-03-16 14:37:47 +0000

    added toolsSidebar facade and bean
    changed QC toold sidebar part to take tools from db entries

--- src/java/toolstuff/FastQCJob.java
+++ src/java/toolstuff/FastQCJob.java
@@ -5,20 +5,55 @@
  */
 package toolstuff;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Fox
  */
 public class FastQCJob extends AbstractJob {
-    
+
     public FastQCJob(String inputPath) {
         getParameters().put("input", inputPath);
-        setExecutableFile("fastqc");
+        setExecutableFile("/Preprocessing/FastQC/fastqc");
     }
-    
+
     @Override
+    //creates html file in input file location with name input_fastqc.html
     public void execute() {
+
+        String filename = getParameters().get("input");
         
+        String command = getCommand() + " " + filename;
+	
+        String output = executeCommand(command);
+
+        //System.out.println(output);
+
     }
-    
+
+    private String executeCommand(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
 }
