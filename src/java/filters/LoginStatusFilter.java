11656b02d2fc705f108d37ac3bdc92605956d067
Fox <Fox@Sigil>
2015-03-17 12:51:53 +0000

    created login filter - redirects all guests trying to access restricted pages to index - redirects users trying to access index or registration to home
    TODO: consider adding some redirect message ie Tried to access restricted page

---
+++ src/java/filters/LoginStatusFilter.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import entitybeans.Users;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Fox
+ */
+@WebFilter(filterName = "LoginStatusFilter", urlPatterns = {"/*"})
+public class LoginStatusFilter implements Filter {
+    
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public LoginStatusFilter() {
+    }
+    
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("LoginStatusFilter:DoBeforeProcessing");
+        }
+
+        // Write code here to process the request and/or response before
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log items on the request object,
+        // such as the parameters.
+	/*
+         for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
+         String name = (String)en.nextElement();
+         String values[] = request.getParameterValues(name);
+         int n = values.length;
+         StringBuffer buf = new StringBuffer();
+         buf.append(name);
+         buf.append("=");
+         for(int i=0; i < n; i++) {
+         buf.append(values[i]);
+         if (i < n-1)
+         buf.append(",");
+         }
+         log(buf.toString());
+         }
+         */
+    }
+    
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("LoginStatusFilter:DoAfterProcessing");
+        }
+
+	// Write code here to process the request and/or response after
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log the attributes on the
+        // request object after the request has been processed. 
+	/*
+         for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
+         String name = (String)en.nextElement();
+         Object value = request.getAttribute(name);
+         log("attribute: " + name + "=" + value.toString());
+
+         }
+         */
+        // For example, a filter might append something to the response.
+	/*
+         PrintWriter respOut = new PrintWriter(response.getWriter());
+         respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
+         */
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String uri = httpRequest.getRequestURI();
+        Users user = null;
+        HttpSession session = httpRequest.getSession();
+        
+        if (session != null) {
+            user = (Users) session.getAttribute("user");
+        }
+
+        //user is logged in, no register, no index pages
+        if (user != null) {
+            if (uri.contains("faces/index.xhtml")
+                    || uri.contains("faces/register.xhtml")) {
+                httpResponse.sendRedirect("faces/home.xhtml");
+            } else {
+                //do not filter other pages
+                chain.doFilter(request, response);
+            }
+        } //user is not logged in, only index and register pages available
+        else {
+            if (uriInaccessibleForGuest(uri)) {
+                httpResponse.sendRedirect("faces/index.xhtml");
+            } else {
+                //do not filter other pages
+                chain.doFilter(request, response);
+            }
+        }
+    }
+    
+    private boolean uriInaccessibleForGuest(String uri) {
+        if (uri.contains("faces/data_item.xhtml")
+                || uri.contains("faces/home.xhtml")
+                || uri.contains("faces/new_job.xhtml")
+                || uri.contains("faces/project.xhtml")
+                || uri.contains("faces/projects_menu.xhtml")
+                || uri.contains("faces/workgroup.xhtml")
+                || uri.contains("faces/workgroups_menu.xhtml")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("LoginStatusFilter:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("LoginStatusFilter()");
+        }
+        StringBuffer sb = new StringBuffer("LoginStatusFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+    
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+        
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+    
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+    
+}
