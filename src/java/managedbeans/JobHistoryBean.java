6e8fe71ea9235264cb314eec7021f4eb23a1ca51
pitas <pitas@Ubuntu12.04LTS>
2015-03-17 13:00:13 +0000

    Job history bean and facade added

---
+++ src/java/managedbeans/JobHistoryBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import entitybeans.Jobhistory;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import sessionbeans.JobHistoryFacade;
+
+/**
+ *
+ * @author pitas
+ */
+@ManagedBean
+@RequestScoped
+public class JobHistoryBean {
+
+    @EJB
+    private JobHistoryFacade jobHistoryFacade;
+    
+    
+    /**
+     * Creates a new instance of JobHistoryBean
+     */
+    public JobHistoryBean() {
+    }
+    
+    public List<String> getJobCommand() {
+        List<String> list = new ArrayList();
+        List<Jobhistory> jobs = jobHistoryFacade.getAllJobs();
+        for (Jobhistory job:jobs) {
+            list.add(job.getCommandused());
+        }
+        return list;
+    }
+    
+}
---
+++ src/java/sessionbeans/JobHistoryFacade.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionbeans;
+
+import entitybeans.Jobhistory;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author pitas
+ */
+
+@Stateless
+public class JobHistoryFacade extends AbstractFacade<Jobhistory> {
+
+    
+    @PersistenceContext(unitName = "RNAseqPU")
+    private EntityManager em;
+
+    public JobHistoryFacade() {
+        super(Jobhistory.class);
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    /**
+     * It retrieves all the user jobs from the history
+     * @return 
+     */
+    public List<Jobhistory> getAllJobs() {
+        Query q = em.createNamedQuery("Jobhistory.findAll");
+        return q.getResultList();
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
+
+
+
+
