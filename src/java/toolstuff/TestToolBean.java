2b77f7da35ee596a2f6c585b1e03fc50cbec114a
lestelles <lestelles@lestelles-PORTEGE-Z930>
2015-03-20 11:49:11 +0000

    Worked on creating a Velvet new job

--- src/java/toolstuff/TestToolBean.java
+++ src/java/toolstuff/TestToolBean.java
@@ -22,6 +22,10 @@ public class TestToolBean {
     private String windowSize ="";
     private String qualityth ="";
     private String outputFile = "none";
+    private String seqType="";
+    private String kmer="";
+    private String insLen="";
+    private String jobid="";
 
     
     
@@ -41,6 +45,14 @@ public class TestToolBean {
         System.out.println("##########################################");
 
         AbstractJob job = new TrimmomaticJob(getInputPath(), getInputPath2(), getWindowSize(), getQualityth());
+        job.execute();
+        setOutputFile(getInputPath().substring(0, getInputPath().lastIndexOf(".")));
+    }
+    
+    public void runVelvet() {
+        System.out.println("##########################################");
+        //inputPath are left reads and inputPath2 are right reads
+        AbstractJob job = new VelvetJob(getSeqType(), getInputPath(), getInputPath2(), getKmer(), getInsLen(), getJobid());
         job.execute();
         setOutputFile(getInputPath().substring(0, getInputPath().lastIndexOf(".")));
     }
@@ -114,5 +126,62 @@ public class TestToolBean {
     public void setOutputFile(String outputFile) {
         this.outputFile = outputFile;
     }
+
+    /**
+     * @return the seqType
+     */
+    public String getSeqType() {
+        return seqType;
+    }
+
+    /**
+     * @param seqType the seqType to set
+     */
+    public void setSeqType(String seqType) {
+        this.seqType = seqType;
+    }
+
+    /**
+     * @return the kmer
+     */
+    public String getKmer() {
+        return kmer;
+    }
+
+    /**
+     * @param kmer the kmer to set
+     */
+    public void setKmer(String kmer) {
+        this.kmer = kmer;
+    }
+
+    /**
+     * @return the insLen
+     */
+    public String getInsLen() {
+        return insLen;
+    }
+
+    /**
+     * @param insLen the insLen to set
+     */
+    public void setInsLen(String insLen) {
+        this.insLen = insLen;
+    }
+
+    /**
+     * @return the jobid
+     */
+    public String getJobid() {
+        return jobid;
+    }
+
+    /**
+     * @param jobid the jobid to set
+     */
+    public void setJobid(String jobid) {
+        this.jobid = jobid;
+    }
+
     
 }
