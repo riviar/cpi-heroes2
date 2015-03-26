ae3c79637edd030b7991d00c140f35e8b52b5716
Matthew Robinson <matthewjrobinson8premier@gmail.com>
2015-03-26 12:34:49 +0000

    added missing userconverter file from 2 commits earlier

---
+++ src/java/entitybeans/UserConverter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entitybeans;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import sessionbeans.AccountSessionFacade;
+
+@FacesConverter(value = "userConverter", forClass = Users.class)
+
+/**
+ *
+ * @author user
+ */
+public class UserConverter implements Converter {
+
+        @EJB
+    AccountSessionFacade accountFacade;
+    
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String userTextID) {
+        userTextID = userTextID.replace("entitybeans.Users[ idusers=", "");
+        userTextID = userTextID.replace(" ]", "");
+        int userID = Integer.valueOf(userTextID);
+        Users user = accountFacade.find(userID);
+        return user;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value.toString();
+    }
+    
+}
