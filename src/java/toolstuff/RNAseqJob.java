a8568b8d254cafd393501af67b47daf0e7271105
Fox <Fox@Sigil>
2015-04-02 12:01:28 +0100

    Merge origin/asierdev into rafaldevTrue
    Conflicts:
    	nbproject/project.properties
    	src/java/managedbeans/JobHistoryBean.java
    	src/java/sessionbeans/AbstractFacade.java
    	src/java/toolstuff/RNAseqJob.java
    	src/java/toolstuff/TestToolBean.java
    	web/WEB-INF/SymiSunTemplate.xhtml

--- src/java/toolstuff/RNAseqJob.java
+++ src/java/toolstuff/RNAseqJob.java
@@ -1,4 +1,3 @@
- <<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -53,11 +52,14 @@ public class RNAseqJob {
         this.command = "/home/vmuser/CPI/tools/";
     }
 
-    public RNAseqJob(UtilityBean utilityBean, JobHistoryFacade jobHistoryFacade, String jobName) {
+    public RNAseqJob(UtilityBean utilityBean, JobHistoryFacade jobHistoryFacade, FilesFacade filesFacade, ProjectSessionFacade projectFacade, String jobName) {
         this.jobHistoryFacade = jobHistoryFacade;
         this.utilityBean = utilityBean;
+        this.filesFacade = filesFacade;
+        this.projectFacade = projectFacade;
         this.jobName = jobName;
         this.command = "/home/vmuser/CPI/tools/";
+        //this.command = "/home/pitas/SOAPdenovo-Trans";
     }
 
     /**
